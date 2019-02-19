ca76657a70291f91e2f8636fe088b3cf9f645ea3
1459177541 <1459177541@qq.com>
2019-02-19 19:47:58 +0800

    更换数据源->druid

--- src/main/java/config/ContextConfig.java
+++ src/main/java/config/ContextConfig.java
@@ -1,6 +1,7 @@
 package config;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.alibaba.druid.filter.logging.Slf4jLogFilter;
+import com.alibaba.druid.pool.DruidDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
@@ -11,12 +12,12 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
+import java.sql.SQLException;
 import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = {"aop", "dao", "entity", "service"})
-@PropertySource({"classpath:jdbc.properties"})
+@PropertySource({"classpath:ApplicationContext.properties"})
 @EnableAspectJAutoProxy(exposeProxy = true)
 @EnableTransactionManagement
 public class ContextConfig{
@@ -29,12 +30,39 @@ public class ContextConfig{
     }
 
     @Bean
-    public DataSource dataSource() throws PropertyVetoException {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass(environment.getProperty("jdbc.driverClass"));
-        dataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
-        dataSource.setUser(environment.getProperty("jdbc.username"));
+    public Slf4jLogFilter slf4jLogFilter(){
+        Slf4jLogFilter filter = new Slf4jLogFilter();
+        filter.setStatementExecutableSqlLogEnable(true);
+        filter.setStatementSqlPrettyFormat(true);
+        return filter;
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.driverClass"));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.username"));
         dataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        dataSource.setFilters("stat,slf4jLogFilter,wall");
+
+        dataSource.setMaxActive(20);
+        dataSource.setInitialSize(1);
+        dataSource.setMaxWait(60000);
+        dataSource.setMinIdle(1);
+
+        dataSource.setTimeBetweenEvictionRunsMillis(60000);
+        dataSource.setMinEvictableIdleTimeMillis(300000);
+
+        dataSource.setValidationQuery("SELECT 'x'");
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setTestOnReturn(false);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
+
+        dataSource.setAsyncInit(true);
         return dataSource;
     }
 
@@ -46,8 +74,8 @@ public class ContextConfig{
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         properties.setProperty("hibernate.show_sql", "true");
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
         properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "create");
         properties.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/ssh");
         properties.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
         factoryBean.setHibernateProperties(properties);
