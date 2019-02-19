30113ac05413b7318215244b598ee5a441940906
1459177541 <1459177541@qq.com>
2019-02-19 17:34:01 +0800

    补充

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+function commit(to){
+	$("input.input").attr("disabled","disabled");
+	var d = JSON.stringify($('form').serializeObject());
+	alert(d);
+	jQuery.ajax({
+		url:to,
+		type:'POST',
+		async:false,
+		contentType : 'application/json',
+		datatype:'json',
+		data:d,
+		success:function(data){
+			alert(data);
+			$("input.input").remove("disabled");
+		},
+		error:function(response, ajaxOptions, thrownError){
+			alert("error");
+		}
+	})
+}
